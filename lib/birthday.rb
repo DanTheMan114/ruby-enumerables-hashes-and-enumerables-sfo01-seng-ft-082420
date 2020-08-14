608feed95c590ca91f19b52eb24284015046522f
Daniel Turner <danielturner184@gmail.com>
2020-08-14 04:05:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,15 @@ suite_c: "Alfred Tennyson",
 suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
-
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
+return passengers[:"Amanda Presley"]
 
 
 
