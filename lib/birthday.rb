1a76fec955484548a49e8a473388a8e2462a108d
Daniel Turner <danielturner184@gmail.com>
2020-08-14 04:04:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,40 +1,9 @@
-
-
-
 # birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
 
-birthday_kids = {
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
-}
 def happy_birthday(birthday_kids)
-  "Happy Birthday #{kids_name}! You are now #{age} years old!"
   # add your code snippet here!
 end
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
-
-
-passengers = {
-suite_a: "Amanda Presley",
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
-}
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
- 
-winner
